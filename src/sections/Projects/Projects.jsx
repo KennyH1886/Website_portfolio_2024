e690adcdfc8adcce773d7165dfc4a1ac94eb5494
KennyH1886 <78048592+KennyH1886@users.noreply.github.com>
2024-11-25 19:46:07 -0500

    added a new project to the website the capital one ai applicaiton from a previous hackathon

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,20 +1,29 @@
 import styles from './ProjectsStyles.module.css';
 import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
 import hipsster from '../../assets/hipsster.png';
 import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
+import squadlobbyPic from '../../assets/squadlobby_logo.jpg'; // Placeholder image
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">New Projects</h1>
+
       <div className={styles.projectsContainer}>
         <ProjectCard
           src={viberr}
           link="https://master--weatherappkh.netlify.app/"
           h3="WeatherAppkh"
           p="Weather app using Api"
+        />
+        
+        
+        <ProjectCard
+          src={squadlobbyPic} 
+          link="https://squad-lobby.aiaggies.net/"
+          h3="Ai Capital One budget Hackathon application"
+          p="An AI application to help manage budgets and track expenses interactively with various scenarios from a Hackathon."
         />
       </div>
     </section>
