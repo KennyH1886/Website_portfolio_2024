06027fcd356be1a27866e341112070fe81c37de3
KennyH1886 <78048592+KennyH1886@users.noreply.github.com>
2025-02-19 17:32:05 -0500

    updated bio

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -23,7 +23,7 @@ function Hero() {
 
   // Typing animation state
   const [displayedText, setDisplayedText] = useState('');
-  const text = "I  have professional experience in Full Stack Development, data science, and research in AI, committed to delivering innovative solutions.";
+  const text = "I have professional experience in Full Stack Development, Data Science, and AI research, committed to delivering innovative solutions. I have worked with national laboratories on cutting-edge projects and collaborated with diverse teams to solve real-world challenges. Additionally, I have won multiple hackathons with many large companies, demonstrating my ability to develop impactful and creative technological solutions under pressure.";
 
   useEffect(() => {
     let index = 0;
