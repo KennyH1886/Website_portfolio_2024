a15a5f2807e51049b237bf341a5f3e9b97403299
KennyH1886 <78048592+KennyH1886@users.noreply.github.com>
2025-09-25 12:01:19 -0400

    updated resume

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -10,7 +10,7 @@ import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
 import linktreeDark from '../../assets/linktree_dark.png'; // Linktree Icon (Dark Mode)
 import profilePic from '../../assets/websitepic.webp';
-import CV from '../../assets/Resume_2025_Kenneth_Howard_Resume_2025.pdf'; // Updated Resume PDF
+import CV from '../../assets/Kenneth_Howard_Resume_F_2025.pdf'; // Updated Resume PDF
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
